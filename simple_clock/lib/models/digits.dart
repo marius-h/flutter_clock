aa23837ac7c7ee7d362109fb3db39cc8441b520c
Laurin Dörre <account@laurindoerre.de>
2020-01-20 23:38:32 +0100

    fixed digits.dart

--- simple_clock/lib/models/digits.dart
+++ simple_clock/lib/models/digits.dart
@@ -9,79 +9,68 @@ List<List<Tuple2<double, double>>> buildDigit(int digit, Point origin, List<List
   switch (digit) {
     case -1:
       pixels = [
-        [ClockState.reset, ClockState.reset],
-        [ClockState.reset, ClockState.reset],
-        [ClockState.reset, ClockState.reset],
+        [ClockState.reset, ClockState.reset,ClockState.reset],
+        [ClockState.reset, ClockState.reset,ClockState.reset],
       ];
       break;
     case 0:
       pixels = [
-        [ClockState.south_east, ClockState.south_west],
-        [ClockState.north_south, ClockState.north_south],
-        [ClockState.north_east, ClockState.north_west],
+        [ClockState.south_east, ClockState.north_south, ClockState.north_east],
+        [ClockState.south_west, ClockState.north_south,ClockState.north_west],
       ];
       break;
     case 1:
       pixels = [
-        [ClockState.idle, ClockState.south_south],
-        [ClockState.idle, ClockState.north_south],
-        [ClockState.idle, ClockState.north_north],
+        [ClockState.idle, ClockState.idle,ClockState.idle],
+        [ClockState.south_south, ClockState.north_south,ClockState.north_north],
       ];
       break;
     case 2:
       pixels = [
-        [ClockState.east_east, ClockState.south_west],
-        [ClockState.south_east, ClockState.north_west],
-        [ClockState.north_east, ClockState.west_west],
+        [ClockState.east_east, ClockState.south_east, ClockState.north_east],
+        [ClockState.south_west, ClockState.north_west,ClockState.west_west],
       ];
       break;
     case 3:
       pixels = [
-        [ClockState.east_east, ClockState.south_west],
-        [ClockState.east_east, ClockState.north_west],
-        [ClockState.east_east, ClockState.north_west],
+        [ClockState.east_east, ClockState.east_east, ClockState.east_east],
+        [ClockState.south_west, ClockState.north_west,ClockState.north_west],
       ];
       break;
     case 4:
       pixels = [
-        [ClockState.south_south, ClockState.south_south],
-        [ClockState.north_east, ClockState.north_south],
-        [ClockState.idle, ClockState.north_north],
+        [ClockState.south_south, ClockState.north_east, ClockState.idle],
+        [ClockState.south_south, ClockState.north_south,ClockState.north_north],
       ];
       break;
     case 5:
       pixels = [
-        [ClockState.south_east, ClockState.west_west],
-        [ClockState.north_east, ClockState.south_west],
-        [ClockState.east_east, ClockState.north_west],
+        [ClockState.south_east, ClockState.north_east, ClockState.east_east],
+        [ClockState.west_west, ClockState.south_west,ClockState.north_west],
       ];
       break;
     case 6:
       pixels = [
-        [ClockState.south_east, ClockState.west_west],
-        [ClockState.north_south, ClockState.south_west],
-        [ClockState.north_east, ClockState.north_west],
+        [ClockState.south_east, ClockState.north_south, ClockState.north_east],
+        [ClockState.west_west, ClockState.south_west,ClockState.north_west],
       ];
       break;
     case 7:
       pixels = [
-        [ClockState.east_east, ClockState.south_west],
-        [ClockState.idle, ClockState.north_south],
-        [ClockState.idle, ClockState.north_north],
+        [ClockState.east_east, ClockState.idle, ClockState.idle],
+        [ClockState.south_west, ClockState.north_south,ClockState.north_north],
       ];
       break;
     case 8:
       pixels = [
-        [ClockState.south_east, ClockState.south_west],
-        [ClockState.north_east, ClockState.south_west],
-        [ClockState.north_east, ClockState.north_west],
+        [ClockState.south_east, ClockState.north_east, ClockState.north_east],
+        [ClockState.south_west, ClockState.south_west,ClockState.north_west],
       ];
       break;
     case 9:
       pixels = [
-        [ClockState.south_east, ClockState.south_west],
-        [ClockState.north_east, ClockState.north_south],
-        [ClockState.east_east, ClockState.north_west],
+        [ClockState.south_east, ClockState.north_east, ClockState.idle],
+        [ClockState.south_west, ClockState.north_south,ClockState.north_north],
       ];
       break;
   }
@@ -95,7 +84,7 @@ List<List<Tuple2<double, double>>> _moveToPoint(
   for (var i = 0; i < pixels.length; ++i) {
     for (var j = 0; j < pixels[i].length; ++j) {
       var o = pixels[i][j];
-      grid[origin.y + j][origin.x + i] = o;
+      grid[origin.x + i][origin.y + j] = o;
     }
   }
   return grid;
--- simple_clock/lib/services/time_provider.dart
+++ simple_clock/lib/services/time_provider.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class TimeProvider extends ChangeNotifier {
-  int _firstDigit = 0;
-  int _secondDigit = 0;
-  int _thirdDigit = 0;
-  int _fourthDigit = 0;
+  int _firstDigit = 1;
+  int _secondDigit = 3;
+  int _thirdDigit = 3;
+  int _fourthDigit = 7;
 
   void updateTime(DateTime time) {
-//    _firstDigit = int.parse(time.hour.toString().substring(0, 0));
-//    _secondDigit = int.parse(time.hour.toString().substring(1, 1));
-//    _firstDigit = int.parse(time.minute.toString().substring(0, 0));
-//    _secondDigit = int.parse(time.minute.toString().substring(1, 1));
+   // _firstDigit = int.parse(time.hour.toString().substring(0, 0));
+   // _secondDigit = int.parse(time.hour.toString().substring(1, 1));
+   // _firstDigit = int.parse(time.minute.toString().substring(0, 0));
+   // _secondDigit = int.parse(time.minute.toString().substring(1, 1));
     notifyListeners();
   }
 
